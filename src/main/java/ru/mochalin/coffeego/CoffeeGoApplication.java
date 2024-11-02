b1d31f11432bbf08d8ee44a55c9ca5ba7c937dbc
Mochalin Nikolay <mochalin.nikolay2017@yandex.ru>
2024-11-02 16:55:32 +0300

    init
    Signed-off-by: Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>

---
+++ src/main/java/ru/mochalin/coffeego/CoffeeGoApplication.java
@@ -0,0 +1,13 @@
+package ru.mochalin.coffeego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mochalin/coffeego/CoffeeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mochalin.coffeego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
