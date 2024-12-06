036f55a337801bf3409a28606d722ed1dd8c2f40
Nikolay Mochalin <mochalin.nikolay2017@yandex.ru>
2024-12-06 12:42:57 +0300

    Добавлена загрузка фото на сервер, пока без корректного вывода

---
+++ src/main/java/ru/mochalin/coffeego/Controller/MainController.java
@@ -0,0 +1,25 @@
+package ru.mochalin.coffeego.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.mochalin.coffeego.Repository.ProductRepository;
+
+@Controller
+public class MainController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "admin";
+    }
+}
--- src/main/java/ru/mochalin/coffeego/Controller/ProductController.java
+++ src/main/java/ru/mochalin/coffeego/Controller/ProductController.java
@@ -3,17 +3,43 @@ package ru.mochalin.coffeego.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.mochalin.coffeego.Model.Product;
 import ru.mochalin.coffeego.Repository.ProductRepository;
 
-@Controller
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller("/product")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("products", productRepository.findAll());
-        return "index";
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static/img/products";
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("product", productRepository.findById(id).get());
+        return "edit-product";
+    }
+
+    @PostMapping("/edit")
+    public String edit(Model model, @ModelAttribute("product") Product product, @RequestParam("imageggg") MultipartFile image) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, image.getOriginalFilename());
+        fileNames.append(image.getOriginalFilename());
+        Files.write(fileNameAndPath, image.getBytes());
+        product.setImage(image.getOriginalFilename());
+        productRepository.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("product", new Product());
+        return "create-product";
     }
 }
